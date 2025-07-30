e226ac3a52321c52abc6f63aaf03e60c7540cea4
MatesIgor <igormates@hotmail.com>
2025-07-30 15:53:18 +0100

    Day_05 - Handlers de Tokens

--- srcs/tokenization/create_token_lst.c
+++ srcs/tokenization/create_token_lst.c
@@ -5,31 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: iwietzke <iwietzke@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/27 17:52:23 by iwietzke          #+#    #+#             */
-/*   Updated: 2025/07/27 17:52:23 by iwietzke         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   create_token_lst.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/29 14:41:24 by luarodri          #+#    #+#             */
-/*   Updated: 2025/07/29 14:41:24 by luarodri         ###   ########.fr       */
+/*   Created: 2025/07/30 15:22:40 by iwietzke          #+#    #+#             */
+/*   Updated: 2025/07/30 15:22:40 by iwietzke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-// TODO : Diminuir funções
+// TODO: diminuir linhas.
 
-#include <minishell.h>
-
-#include <minishell.h>
-
+/**
+ * tokenize_input - Função principal de tokenização
+ * @shell: estrutura principal do shell
+ * @input: string de entrada do usuário
+ * 
+ * Percorre a string de entrada e identifica diferentes tipos de tokens:
+ * - Operadores lógicos (&&, ||)
+ * - Pipes (|)
+ * - Parênteses (, )
+ * - Redirecionamentos (<, >, >>, <<)
+ * - Palavras e comandos
+ * 
+ * Cada tipo é processado por seu handler específico
+ */
 void	tokenize_input(t_shell *shell, char *input)
 {
 	int		i;
@@ -37,59 +35,67 @@ void	tokenize_input(t_shell *shell, char *input)
 	i = 0;
 	while (input[i])
 	{
-		if (input[i] == '|' && input[i + 1] == '|')
-			i = handle_or(shell, input, i);
-		else if (input[i] == '|')
-			i = handle_pipe(shell, input, i);
-		else if (input[i] == '(' || input[i] == ')')
-			i = handle_parenthesis(shell, input, i);
-		else if (input[i] == '&' && input[i + 1] == '&')
-			i = handle_andif(shell, input, i);
-		else if (input[i] == '>' || input[i] == '<')
-			i = handle_redir(shell, input, i);
-		else if (ft_isspace(input[i]))
+		// Pula espaços em branco
+		if (ft_isspace(input[i]))
 		{
 			while (ft_isspace(input[i]))
 				i++;
+			continue;
 		}
+		
+		// Verifica operadores lógicos primeiro (&&, ||)
+		if (input[i] == '&' && input[i + 1] == '&')
+			i = handle_andif(shell, input, i);
+		else if (input[i] == '|' && input[i + 1] == '|')
+			i = handle_or(shell, input, i);
+		// Pipe simples (|)
+		else if (input[i] == '|')
+			i = handle_pipe(shell, input, i);
+		// Parênteses (, )
+		else if (input[i] == '(' || input[i] == ')')
+			i = handle_parenthesis(shell, input, i);
+		// Redirecionamentos (<, >, >>, <<)
+		else if (input[i] == '>' || input[i] == '<')
+			i = handle_redir(shell, input, i);
+		// Palavras, comandos e argumentos
 		else
 			i = handle_word_token(shell, input, i);
+		
+		// Atualiza posições dos tokens
 		set_token_pos(shell->token_lst);
 	}
 }
 
+/**
+ * handle_word_token - Processa tokens de palavra/comando
+ * @shell: estrutura principal do shell
+ * @input: string de entrada
+ * @i: posição atual na string
+ * 
+ * Extrai palavras, comandos e argumentos da entrada.
+ * Lida com aspas simples e duplas, preservando conteúdo
+ * e mantendo informação sobre o tipo de aspas usado.
+ * 
+ * Return: nova posição no input após processar a palavra
+ */
 int	handle_word_token(t_shell *shell, char *input, int i)
 {
 	t_token	*new_token;
 	char	*str;
-	int		start;
 
 	new_token = ft_calloc(1, sizeof(t_token));
 	if (!new_token)
 		exit_failure(shell, "handle_word_token");
 	
-	start = i;
+	str = ft_strdup(""); // Inicializa string vazia
+	if (!str)
+		exit_failure(shell, "handle_word_token_str");
 	
-	// Pular até encontrar espaço ou metacharacter
+	// Processa caracteres até encontrar delimitador
 	while (input[i] && !ft_isspace(input[i]) && !ft_ismeta(input, i))
 	{
-		if (ft_isquote(input[i]))
-		{
-			char quote = input[i];
-			i++; // pula a quote inicial
-			while (input[i] && input[i] != quote)
-				i++;
-			if (input[i] == quote)
-				i++; // pula a quote final
-		}
-		else
-			i++;
+		i = join_strs(shell, &str, input, i);
 	}
-	
-	// Extrair a substring
-	str = ft_substr(input, start, i - start);
-	if (!str)
-		exit_failure(shell, "handle_word_token");
 	
 	new_token->value = str;
 	new_token->type = WORD;
@@ -99,56 +105,51 @@ int	handle_word_token(t_shell *shell, char *input, int i)
 	return (i);
 }
 
-int	handle_redir(t_shell *shell, char *input, int i)
-{
-	t_token	*new_token;
-
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (!new_token)
-		exit_failure(shell, "handle_redir");
-	
-	if (input[i] == '>' && input[i + 1] == '>')
-		i = set_append(shell, new_token, input, i);
-	else if (input[i] == '<' && input[i + 1] == '<')
-		i = set_hered(shell, new_token, input, i);
-	else
-		i = set_simple(shell, new_token, input, i);
-	
-	new_token->state = GENERAL;
-	ft_lstadd_back(&shell->token_lst, ft_lstnew(new_token));
-	
-	while (input[i] && ft_isspace(input[i]))
-		i++;
-	
-	return (i);
-}
-
-int	handle_pipe(t_shell *shell, char *input, int i)
-{
-	t_token	*new_token;
-
-	new_token = ft_calloc(1, sizeof(t_token));
-	if (!new_token)
-		exit_failure(shell, "handle_pipe");
-	
-	new_token->value = ft_strdup("|");
-	if (!new_token->value)
-		exit_failure(shell, "handle_pipe_1");
-	
-	new_token->type = PIPE;
-	new_token->state = GENERAL;
-	ft_lstadd_back(&shell->token_lst, ft_lstnew(new_token));
-	
-	i++;
-	while (input[i] && ft_isspace(input[i]))
-		i++;
-	
-	return (i);
-}
-
+/**
+ * handle_quotes - Processa especificamente tokens com aspas
+ * @shell: estrutura principal do shell
+ * @input: string de entrada
+ * @i: posição atual na string
+ * 
+ * Função especializada para processar strings que começam com aspas.
+ * Define o estado do token baseado no tipo de aspas:
+ * - IN_SQUOTES para aspas simples
+ * - IN_DQUOTES para aspas duplas
+ * 
+ * Return: nova posição no input após processar as aspas
+ */
 int	handle_quotes(t_shell *shell, char *input, int i)
 {
-	// Por enquanto, vamos tratar quotes como words normais
-	// Isso será refinado quando implementarmos expansão
-	return (handle_word_token(shell, input, i));
+	t_token		*new_token;
+	const char	quote = input[i];
+	char		*str;
+
+	new_token = ft_calloc(1, sizeof(t_token));
+	if (!new_token)
+		exit_failure(shell, "handle_quotes");
+	
+	str = ft_strdup("");
+	if (!str)
+		exit_failure(shell, "handle_quotes_str");
+	
+	// Processa conteúdo incluindo aspas
+	while (input[i] && !ft_isspace(input[i]) && !ft_ismeta(input, i))
+	{
+		i = join_strs(shell, &str, input, i);
+	}
+	
+	new_token->value = str;
+	new_token->type = WORD;
+	
+	// Define estado baseado no tipo de aspas
+	if (quote == '"')
+		new_token->state = IN_DQUOTES;
+	else if (quote == '\'')
+		new_token->state = IN_SQUOTES;
+	else
+		new_token->state = GENERAL;
+	
+	ft_lstadd_back(&shell->token_lst, ft_lstnew(new_token));
+	
+	return (i);
 }
